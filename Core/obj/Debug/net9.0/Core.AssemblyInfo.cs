c24a6a9267c035c7c8b4efde1af1accc3f610ee0
Kostis Grf <kostisgratsos@hotmail.gr>
2025-02-13 05:53:33 +0200

    implemented repository pattern

--- Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82b6abc2cdd1752d3275f6c78cdb0f39f369a353")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+519f313966500c655d473dc450ab70976371e0b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

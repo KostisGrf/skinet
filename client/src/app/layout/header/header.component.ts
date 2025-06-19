715554a33b143207da7ed1dd58c3f30baddb88a3
Kostis Grf <kostisgratsos@hotmail.gr>
2025-06-19 05:02:22 +0300

    Builds UI

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-import {MatIcon} from '@angular/material/icon';
-import {MatButton} from '@angular/material/button';
-import {MatBadge} from '@angular/material/badge';
-
+import {MatIcon} from "@angular/material/icon";
+import {MatButton} from "@angular/material/button";
+import {MatBadge} from "@angular/material/badge";
 
 @Component({
   selector: 'app-header',

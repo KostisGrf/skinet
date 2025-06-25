749ebe5d61ada01b7bf82d10ed2ec7b7e978f690
Kostis Grf <kostisgratsos@hotmail.gr>
2025-06-25 05:53:14 +0300

    Implement identity on back end

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,34 +1,51 @@
 using System;
+using System.Security.Claims;
 using API.DTOs;
 using Core.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
-public class BuggyController:BaseApiController
+public class BuggyController : BaseApiController
 {
     [HttpGet("unauthorized")]
-    public IActionResult GetUnauthorized(){
+    public IActionResult GetUnauthorized()
+    {
         return Unauthorized();
     }
 
     [HttpGet("badrequest")]
-    public IActionResult GetBadRequest(){
-         return BadRequest("not a good request");
+    public IActionResult GetBadRequest()
+    {
+        return BadRequest("not a good request");
     }
 
     [HttpGet("notfound")]
-    public IActionResult GetNotFound(){
+    public IActionResult GetNotFound()
+    {
         return NotFound();
     }
 
     [HttpGet("internalerror")]
-    public IActionResult GetInternalError(){
+    public IActionResult GetInternalError()
+    {
         throw new Exception("this is a test exception");
     }
 
     [HttpPost("validationerror")]
-    public IActionResult GetValidationError(CreateProductDto product){
+    public IActionResult GetValidationError(CreateProductDto product)
+    {
         return Ok();
+    }
+
+    [Authorize]
+    [HttpGet("secret")]
+    public IActionResult GetSecret()
+    {
+        var name = User.FindFirst(ClaimTypes.Name)?.Value;
+        var id = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        return Ok("Hello " + name + " with the id of " + id);
     }
 }

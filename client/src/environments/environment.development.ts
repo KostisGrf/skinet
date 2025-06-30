9b7a2336104dea0a98756f76f8b951d76a59ffec
Kostis Grf <kostisgratsos@hotmail.gr>
2025-06-30 04:04:26 +0300

    Implements Checkout

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production:false,
-    apiUrl:'https://localhost:5001/api/'
+    apiUrl:'https://localhost:5001/api/',
+    stripePublicKey:'pk_test_51ReRWiPDdx7fNVrIGLo8lev92oitqeuNofEYwOyuQyGo9TRQjyeexvq49FlegzonRtnM7yvX4zv7Yvvj1d3EU95n00R8EZTii2'
 };

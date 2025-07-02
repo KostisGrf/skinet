6386ff710e9cf550b76d94f35b8a137fdd7a9af3
Kostis Grf <kostisgratsos@hotmail.gr>
2025-07-02 04:41:56 +0300

    Implements order,payment and websockets

--- client/src/app/features/checkout/checkout-success/checkout-success.component.ts
+++ client/src/app/features/checkout/checkout-success/checkout-success.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { SignalrService } from '../../../core/services/signalr.service';
+import { AddressPipe } from '../../../shared/pipes/address.pipe';
+import { CurrencyPipe, DatePipe, NgIf } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { PaymentPipe } from '../../../shared/pipes/payment.pipe';
+import { OrderService } from '../../../core/services/order.service';
 
 @Component({
   selector: 'app-checkout-success',
   imports: [
     MatButton,
-    RouterLink
+    RouterLink,
+    MatProgressSpinnerModule,
+    AddressPipe,
+    CurrencyPipe,
+    DatePipe,
+    PaymentPipe,
+    NgIf
+
   ],
   templateUrl: './checkout-success.component.html',
   styleUrl: './checkout-success.component.scss'
 })
-export class CheckoutSuccessComponent {
+export class CheckoutSuccessComponent implements OnDestroy  {
+   private signalRService=inject(SignalrService);
+   private orderService=inject(OrderService);
+   order=this.signalRService.orderSignal();
 
+   ngOnDestroy(): void {
+       this.orderService.orderComplete=false;
+       this.signalRService.orderSignal.set(null);
+   }
+
+   
 }
--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production:false,
     apiUrl:'https://localhost:5001/api/',
+    hubUrl:'https://localhost:5001/hub/notifications',
     stripePublicKey:'pk_test_51ReRWiPDdx7fNVrIGLo8lev92oitqeuNofEYwOyuQyGo9TRQjyeexvq49FlegzonRtnM7yvX4zv7Yvvj1d3EU95n00R8EZTii2'
 };

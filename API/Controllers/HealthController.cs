9b19b65995dd11ac0a0f55a3d4d94f7d77b5fdcb
Kostis Grf <kostisgratsos@hotmail.gr>
2025-07-10 05:55:02 +0300

    Add lightweight health check endpoint

---
+++ API/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Healthy");
+    }
+}
